5fc20a34fd3b1a96ff0e842cffe30dc97b205a19
padmavathi <yenugupadmavathi@gmail.com>
2017-08-22 21:48:48 +0530

    display the data

---
+++ Retailproject/src/com/MainTest.java
@@ -0,0 +1,21 @@
+package com;
+
+import com.Retaildata;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+			Retaildata rd=new Retaildata();
+			 greeting();
+		rd.display();
+			
+	}
+	private static void greeting() {
+			System.out.println("hello");
+			System.out.println("hi");
+
+			
+
+	}
+
+}
---
+++ Retailproject/src/com/Retaildata.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Retaildata {
+		public void display()
+		{
+			System.out.println("hello");
+		}
+
+} 
+
+
